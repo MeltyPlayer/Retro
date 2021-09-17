6e91a2e397c1b57925ec2ecfdb35ca9b050e7066
MeltyPlayer <mastergamepro@hotmail.com>
2021-09-17 01:47:40 -0500

    Added Forest of Hope.

---
+++ Assets/engines/pikmin/olimar/OlimarController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OlimarController : MonoBehaviour {
+  private Camera camera_;
+
+  // Start is called before the first frame update
+  void Start() {
+    this.camera_ = Camera.main;
+  }
+
+  // Update is called once per frame
+  void Update() {
+  }
+}
